770fec13f7d259826cd00ab67d991e2bc802f311
GuiRod09 <guirodrigues059@gmail.com>
2025-11-18 21:04:01 -0300

    criei a solucao get do supplier

---
+++ src/lib/app/api/suppliers/[id]/route.ts
@@ -0,0 +1,26 @@
+    import { NextResponse, NextRequest } from 'next/server';
+import { prisma } from "./../../../../lib/prisma";
+import { Prisma } from '@prisma/client';
+
+interface Params {
+  id: string;
+}
+
+export async function GET(request: NextRequest, context: { params: Params }) {
+  const { id } = context.params;
+
+  try {
+    const supplier = await prisma.supplier.findUnique({
+      where: { id },
+    });
+
+    if (!supplier) {
+      return NextResponse.json({ error: 'Supplier not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(supplier);
+  } catch (error) {
+    console.error('Error fetching supplier:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
